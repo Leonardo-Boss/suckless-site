4691f89e29ffd90f062e7d54decb9a05985b7f14
Hiltjo Posthuma <hiltjo@codemadness.org>
2019-02-07 20:49:58 +0100

    add program based on smu to print markdown links + script to check internal wiki links

---
+++ checklinks.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# check internal wiki links, uses md-printlinks.c
+
+for t in */; do
+find "$t" -name "*.md" -type f | while read -r f; do
+	d=$(dirname "$f")
+	b=$(basename "$f")
+
+	./md-printlinks < "$f" | \
+		grep -vE '^(http|https|gopher|irc|ircs|git)://' | \
+		grep -vE '^\/\/(lists|dl|git)\.' | \
+		grep -vE '^mailto:' | \
+		while read -r -- l; do
+
+		# // relative
+		p="${l#//}"
+		if test x"$p" != x"$l"; then
+			bp="${p%%/*}"
+			# topmost dir doesn't exist, possibly // url to other site.
+			if ! test -d "$bp"; then
+				echo "$f	$l	$bp"
+				continue
+			fi
+			path="$p"
+		else
+			p="${l#/}"
+			if test x"$l" != x"$p"; then
+				# prefix is "/", use topmost dir + path.
+				d="$t"
+			fi
+			path="$d/$l"
+		fi
+
+		test -e "$path" || echo "$f	$l	$path"
+	done
+done
+done
